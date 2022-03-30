80413b84a47f0caf646b10fce4d16f6d5ff467ac
Rajat Baldawa <rbaldawa@wpi.edu>
2022-03-29 21:23:04 -0400

    commit

--- app/src/main/java/com/example/petpicker/MainActivity.java
+++ app/src/main/java/com/example/petpicker/MainActivity.java
@@ -3,19 +3,38 @@ package com.example.petpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    //hi this is alexis :)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Hey Kidz, Isaiah was here, watching you
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
 
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //User and password is admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"Access Authorized",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"Unable to Authorize Access",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-    //Rajat Test #1
-    //Rajat Test #2
-    //Rajat Test #3
+
 }
