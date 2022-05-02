5db4111162d532cf7c29f96832ab425f408698d3
rbaldawa28 <rbaldawa@wpi.edu>
2022-05-02 12:56:42 -0400

    Activities working now hopefully

--- app/src/main/java/com/example/petpicker/Option4.java
+++ app/src/main/java/com/example/petpicker/Option4.java
@@ -1,6 +1,14 @@
 package com.example.petpicker;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Option4 extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option4);
+
+    }
 }
--- app/src/main/java/com/example/petpicker/Option5.java
+++ app/src/main/java/com/example/petpicker/Option5.java
@@ -1,6 +1,14 @@
 package com.example.petpicker;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Option5 extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option5);
+
+    }
 }
